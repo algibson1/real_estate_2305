1edbfdabe8824c425b28744cb2bb36da10ac2f5f
Artemy Gibson <artemygibson@gmail.com>
2023-05-18 09:12:25 -0700

    Iteration 4: Add Iteration 4 methods and pass tests

--- lib/house.rb
+++ lib/house.rb
@@ -27,4 +27,16 @@ class House
     def details
         {"price" => @price, "address" => @address}
     end
+
+    def price_per_square_foot
+        (@price.to_f/area).round(2)
+    end
+
+    def rooms_sorted_by_area
+        @rooms.sort_by {|room| room.area}.reverse
+    end
+
+    def rooms_by_category
+        @rooms.group_by {|room| room.category}
+    end
 end
