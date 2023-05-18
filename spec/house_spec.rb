775cfe68dbd55a9085edfe098ee470bc50676c11
Artemy Gibson <artemygibson@gmail.com>
2023-05-18 08:38:59 -0700

    Iteration 2: Write tests for add_room method

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe House do
     describe "Iteration 2" do
         before do
             @house = House.new("$400000", "123 sugar lane")
-
+            @room_1 = Room.new(:bedroom, 10, '13')
+            @room_2 = Room.new(:bedroom, 11, '15')
         end
     
         it 'exists' do
@@ -24,5 +25,10 @@ RSpec.describe House do
             expect(@house.rooms).to eq([])
         end
 
+        it 'can add rooms' do
+            @house.add_room(@room_1)
+            @house.add_room(@room_2)
+            expect(@house.rooms).to eq([@room_1, @room_2])
+        end
     end
 end
