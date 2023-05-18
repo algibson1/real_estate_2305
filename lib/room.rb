b7579d336af53d2fc6be34a386fd33f97f90a334
Artemy Gibson <artemygibson@gmail.com>
2023-05-18 08:27:46 -0700

    Iteration 1: Finish room class

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,16 @@
 class Room
+  attr_reader :category, :area
+  def initialize(category, length, width)
+    @category = category
+    @area = length * width.to_i
+    @painted = false
+  end
 
-  def initialize
+  def is_painted?
+    @painted
+  end
 
+  def paint
+    @painted = true
   end
 end
